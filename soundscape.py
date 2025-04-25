f7f6e451aaa7f7e5305a57425f68d6d8ccd6345b
Michiel Bontenbal <michiel.bontenbal@gmail.com>
2025-04-25 15:46:13 +0200

    soundscape en cleanup duckdb

---
+++ soundscape.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# A document with some soundscapes 
+# See for mor information: https://en.wikipedia.org/wiki/Soundscape
+
+marineterrein_labels = [
+        "Airco",
+        "Airplane",
+        "Alarm",
+        "Birds",
+        "Car",
+        "Church bell",
+        "Claxon",        
+        "Gunshot",
+        "Helicopter",        
+        "Moped",
+        "Motorcycle",
+        "Music",
+        "Noise",
+        "Screaming",
+        "Silence",
+        "Siren",
+        "Slamming door",
+        "Talking"
+    ]
+
+my_home_labels = [
+        "Airco",
+        "Alarm",
+        "Birds",
+        "Car",,
+        "Claxon",        
+        "Helicopter",        
+        "Moped",
+        "Motorcycle",
+        "Music",
+        "Noise",
+        "Piano",
+        "Silence",
+        "Siren",
+        "Slamming door",
+        "Steps",
+        "Talking",
+        "Tram",
+        "Vacuum cleaner",
+        "Washing machine",
+    ]
