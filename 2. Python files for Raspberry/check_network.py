b9e8c7e207585dcc51371e900f580d3176399ae2
Michiel Bontenbal <michiel.bontenbal@gmail.com>
2025-05-03 16:41:19 +0200

    checknetwork

--- 2. Python files for Raspberry/check_network.py
+++ 2. Python files for Raspberry/check_network.py
@@ -1,6 +1,14 @@
-import psutil
-print(psutil.net_if_stats())
-
 import subprocess
 result = subprocess.run(['iwconfig'], capture_output=True, text=True)
-print(result)
+output = result.stdout
+#print(output)
+
+print(50*'-')
+for line in output.splitlines():
+    if 'Signal level' in line:
+        print(line.strip())
+    if 'ESSID' in line:
+        print(line.strip())
+    else:
+        pass
+print(50*'-')
